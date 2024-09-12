714bad3bc2d7ff9c2efbd4ff24d88049575a1c49
Hans Hazwan <muhammadhazwanzufar79@gmail.com>
2024-09-12 20:41:53 +0800

    Make it work.

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,48 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
+use std::io::{BufRead, BufReader, Write};
 use std::thread;
+use std::env;
 use std::fs;
 
+const HELLO_PAGE_FILENAME: &str = "hello.html";
+
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    let request_line = http_request.get(0).unwrap();
+    let request_uri = request_line.split(" ").nth(1).unwrap();
 
-    match stream.read(&mut buffer) {
-        Ok(bytes_read) => {
-            let http_request = String::from_utf8_lossy(&buffer[..bytes_read]);
+    log::info!("HTTP Request: {:#?}\nRequest URI: {}", http_request, request_uri);
 
-            match http_request.lines().next() {
-                Some(line_request) => {
-                    let request_uri = line_request.split(" ").nth(1).unwrap();
-                    let message = request_uri.split("/").nth(1).unwrap();
-                    let message = message.replace("%20", " ");
+    let message = request_uri.strip_prefix("/").unwrap().replace("%20", " ");
+    let content = format!("<h1>Hello {} !!!</h1>", message);
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", content.len(), content);
 
-                    println!("Message: {}", message);
-
-                    let content = fs::read_to_string("index.html").unwrap();
-                    let content = content.replace("{}", &message);
-                    let response = format!("HTTP/1.1 OK 200\r\nContent-Length: {}\r\n\r\n{}", content.len(), content);
-
-                    stream.write_all(response.as_bytes()).unwrap();
-                },
-                None => {
-                    println!("Error: The request are empty.");
-                },
-            }
-        },
-        Err(err) => {
-            println!("Error: {:?}", err);
-        }
-    }
+    stream.write_all(response.as_bytes()).unwrap();
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env::set_var("RUST_LOG", "DEBUG");
+    env_logger::init();
+
     let listener = TcpListener::bind("127.0.0.1:8080")?;
-    println!("Listening server on http://localhost:8080");
+    log::info!("Running server on http://localhost:8080");
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
+                log::info!("New connection !!!");
+
                 thread::spawn(move || {
                     handle_connection(stream);
                 });
             },
             Err(err) => {
-                println!("Error: {:?}", err);
+                log::error!("Connection failed: {:#?}", err);
             }
         }
     }
